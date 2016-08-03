2455ff5bd1f5dea287965b17269df64d81ce6dd5
Jakub Matejka <jakub@keboola.com>
2016-08-03 10:07:37 +0200

    feat: signup to user pool

--- auth/signup/handler.js
+++ auth/signup/handler.js
@@ -5,19 +5,6 @@ var aws = require('aws-sdk');
 var jwt = require('../../lib/jwt');
 var response = require('../../lib/response');
 
-var CognitoHelper = require('../../lib/cognito-helper/cognito-helper');
-var cognito = new CognitoHelper({
-  AWS_ACCOUNT_ID: process.env.AWS_ACCOUNT_ID,
-  COGNITO_IDENTITY_POOL_ID: process.env.COGNITO_IDENTITY_POOL_ID,
-  COGNITO_DEVELOPER_PROVIDER_NAME: process.env.COGNITO_DEVELOPER_PROVIDER_NAME,
-  COGNITO_SEPARATOR: process.env.COGNITO_SEPARATOR || '----',
-  COGNITO_DATASET_NAME: process.env.COGNITO_DATASET_NAME || 'profile',
-  COGNITO_PASSWORD_RESET_URL: process.env.COGNITO_PASSWORD_RESET_URL || 'http://localhost:8100/app.html#/reset/{email}/{reset}',
-  COGNITO_PASSWORD_RESET_BODY: process.env.COGNITO_PASSWORD_RESET_BODY || 'Dear {name}, please follow the link below to reset your password:',
-  COGNITO_PASSWORD_RESET_SUBJECT: process.env.COGNITO_PASSWORD_RESET_SUBJECT || 'Password reset',
-  COGNITO_PASSWORD_RESET_SOURCE: process.env.COGNITO_PASSWORD_RESET_SOURCE || 'Password reset <noreply@yourdomain.com>'
-});
-
 var db = require('../../lib/db');
 
 var vandium = require('vandium');
@@ -47,26 +34,25 @@ module.exports.handler = vandium(function (event, context, callback) {
       });
     },
     function(callbackLocal) {
-      cognito.signup(event.name, event.email, event.password, event.vendor, function (err) {
-        return callbackLocal(err);
-      });
-    },
-    function (callbackLocal) {
-      delete event.password;
-      var ses = new aws.SES({apiVersion: '2010-12-01', region: process.env.SERVERLESS_REGION});
-      ses.sendEmail({
-        Source: process.env.SES_EMAIL,
-        Destination: { ToAddresses: [process.env.SES_EMAIL] },
-        Message: {
-          Subject: {
-            Data: '[dev-portal] User ' + event.email + ' requests approval'
+      var provider = new aws.CognitoIdentityServiceProvider({region: process.env.SERVERLESS_REGION});
+      provider.signUp({
+        ClientId: process.env.COGNITO_USER_IDENTITY_POOL_CLIENT_ID,
+        Username: event.email,
+        Password: event.password,
+        UserAttributes: [
+          {
+            Name: 'email',
+            Value: event.email
           },
-          Body: {
-            Text: {
-              Data: JSON.stringify(event, null, 4)
-            }
+          {
+            Name: 'name',
+            Value: event.name
+          },
+          {
+            Name: 'profile',
+            Value: event.vendor
           }
-        }
+        ]
       }, function(err) {
         return callbackLocal(err);
       });
